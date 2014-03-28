7919ba9aa3bf6eac68ca7d5a92042428cd320c1a
Dmitry S. Makovey <dimon@rns.phs.net>
2014-03-28 14:32:00 -0600

    enabled templating for pages

--- wiki.py
+++ wiki.py
@@ -1,5 +1,6 @@
 #!/bin/env python
 
+# import pdb; pdb.set_trace()
 import os
 import sys
 
@@ -7,14 +8,26 @@ import docutils.core
 import re
 import glob
 
-from flask import Flask, Response, send_file, render_template_string, render_template
+from flask import Flask, Response, send_file, render_template_string, render_template, stream_with_context, Markup
 from jinja2 import TemplateNotFound
+from flask_debugtoolbar import DebugToolbarExtension
 
 wiki_base='content'
 wiki_compiled='compiled'
 
 # wiki_tag_re=re.compile(r'(?P<pre>[^\[])\[(?P<wikiword>.*)\|?(?P<humanword>.*?)\](?P<post>[^\]])')
 wiki_tag_re=re.compile(r'(?<!\[)\[(?P<wikiword>[\w/]+)\|?(?P<humanword>.*?)\](?<=\])')
+
+dir_list_template="""
+<ul>
+{% for node,alias in node_list %}
+<li><a href="/{{ node }}">{{ alias }}</a></li>
+{% endfor %}
+</ul>
+"""
+
+page_template="""<body>{% for l in body %}{{ l }}{% endfor %}</body> """
+
 
 class WikiNotImplemented(Exception):
     pass
@@ -26,6 +39,9 @@ class Wiki(object):
 
 wiki=Wiki(wiki_base,wiki_compiled)
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'EmbUd/dielMWkrPlY0Dy9szv6X0fvKgX4vCfBaDJOFAmdx6pZDx6A'
+app.config['DEBUG_TB_TEMPLATE_EDITOR_ENABLED']=True
+toolbar = DebugToolbarExtension(app)
 
 # render_template
 # render_template_string
@@ -36,14 +52,16 @@ app = Flask(__name__)
 def stream_template(template_name, **context):
     app.update_template_context(context)
     t = app.jinja_env.get_template(template_name)
-    rv = t.stream(context)
-    rv.enable_buffering(5)
+    # rv = t.stream(context)
+    rv = t.generate(context)
+    # rv.enable_buffering(5)
     return rv
 
 def stream_string_template(template_string, **context):
     app.update_template_context(context)
     t = app.jinja_env.from_string(template_string)
     rv = t.stream(context)
+    # rv = t.render(context)
     rv.enable_buffering(5)
     return rv
 
@@ -64,9 +82,14 @@ class WikiFile(object):
     def compiled_iterator(self):
         with open(self.getCompiledPath(),'r') as f:
             for l in f:
-                yield l
+                # Disable autoescaping of returned code by
+                # jinja 
+                yield Markup(l)
     
-    def render(self):
+    def render(self,template=None):
+        """
+        @template - template file name
+        """
         fs_compiled_path=self.getCompiledPath()
         fs_content_path=self.getContentPath()
         if not os.path.exists(fs_compiled_path):
@@ -75,8 +98,14 @@ class WikiFile(object):
         elif os.path.getmtime(fs_compiled_path)<os.path.getmtime(fs_content_path):
             app.logger.debug('force-compiling (too old): '+self.path)
             self.compile()
-        return self.compiled_iterator()
-    
+        if template:
+            try:
+                return stream_template(template,body=self.compiled_iterator())
+            except TemplateNotFound:
+                # ok, not found template, fine, fallback to default
+                pass
+        return stream_string_template(page_template,body=self.compiled_iterator())
+        # return self.compiled_iterator()
     
     def unwiki(self,content):
         unwiki=wiki_tag_re.sub(r'<a href="\g<wikiword>">\g<humanword></a>',content)
@@ -137,15 +166,6 @@ class RstFile(PlainFile):
         with open(fs_compiled_path,'w') as compiled:
             compiled.write(self.unwiki(parts['html_body']))
 
-dir_list_template="""
-<ul>
-{% for node,alias in node_list %}
-<li><a href="/{{ node }}">{{ alias }}</a></li>
-{% endfor %}
-</ul>
-"""
-
-page_template="""{{ contents }}"""
 
 def dir_listing(nodes,aliases=None):
     if aliases:
@@ -155,9 +175,13 @@ def dir_listing(nodes,aliases=None):
     node_list=zip(nodes,names)
     try:
         node_list=zip(nodes,names)
-        return render_template('dirlist.html',node_list=node_list)
+        res=render_template('dirlist.html',node_list=node_list)
+        # app.logger.debug(res)
+        return res
     except TemplateNotFound:
-        return render_template_string(dir_list_template,node_list=node_list)
+        res=render_template_string(dir_list_template,node_list=node_list)
+        # app.logger.debug(res)
+        return res
 
 def listdir(fs_path,prepend=None):
     res=[]
@@ -174,6 +198,7 @@ def render_file(wiki_path):
     fn,fe=os.path.splitext(fs_path)
     page=None
     content=None
+    template_name='page.html'
     if fe in ['.rst','.rest']:
         page=RstFile(wiki_path,wiki)
     elif fe in ['.txt',]:
@@ -183,7 +208,10 @@ def render_file(wiki_path):
     elif os.path.exists(fs_path):
         content=send_file(fs_path)
     if page:
-        return Response(page.render(),mimetype='text/html')
+        # we don't want to be rude and disable ALL autoescaping
+        ## app.jinja_env.autoescape=False
+        return Response(stream_with_context(page.render(template_name)),mimetype='text/html')
+        ## app.jinja_env.autoescape=True
     elif content:
         return content
 
@@ -217,4 +245,4 @@ def serve_node(node_name):
     return res
 
 if __name__ == '__main__':
-    app.run(debug=True,host='127.0.0.1',port=8000)
+    app.run(debug=True,use_debugger=True,host='127.0.0.1',port=8000)
